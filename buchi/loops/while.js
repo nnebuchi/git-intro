65e9a9df9950d2f98ba1a433a66ad79b4c4fd340
nnebuchi <nnebuchiosigbo340@gmail.com>
2023-10-26 12:25:42 +0100

    foreach loop

---
+++ buchi/loops/while.js
@@ -0,0 +1,27 @@
+// print numbers from 1 to 10
+let x = 1;
+while (x <= 10) {
+// console.log(x);
+    x = x+1
+}
+
+// Write a while loop that prints the squares of numbers from 1 to 10.
+
+let y = 1;
+while (y <=10) {
+    // console.log(y**2);
+    y++;
+}
+
+// Write a while loop that counts down from 10 to 1.
+let z = 10;
+while(z >= 1){
+    console.log(z);
+    z = z-1;
+}
+
+// Write a while loop that prints squares of numbers from 100 to 5
+
+// Assignment
+
+// Exercise 5: Write a while loop that prints the Fibonacci sequence up to the 10th term.
