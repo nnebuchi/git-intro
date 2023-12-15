7c0670af5c15d09225926509f898eab00a9cdca0
nnebuchi <nnebuchiosigbo340@gmail.com>
2023-12-15 15:08:01 +0100

    dom manaipulation, conditionals

--- buchi/dom/manipulation/computed_styles.js
+++ buchi/dom/manipulation/computed_styles.js
@@ -1,3 +1,15 @@
-const navStyles = getComputedStyle(document.querySelector(".navbar"))
+const navbar = document.getElementById("navbar")
+const navStyles = getComputedStyle(navbar)
 const navBg = navStyles.backgroundColor
 console.log(navBg);
+const tableHeading = document.querySelector("h2")
+tableHeading.style.color='blue';
+tableHeading.style.fontSize = '50px'
+
+//textContent
+// innerHtml
+// remove()
+// append
+// prepend
+
+// document.querySelector(".navbar").style.backgroundColor='blue'
